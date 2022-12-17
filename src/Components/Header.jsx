0d71cdbc2b486246ce9b6e74418c3f8ed5af40fa
Camila Forero <amandaforero@outlook.com>
2022-12-16 22:21:49 -0500

    Se agrega icono de carrito de compras

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Logo from '../img/Delicias Almaleka.png'
 import '../Styles/Header.css'
-import {GiHamburgerMenu} from "react-icons/gi";
+import {GiHamburgerMenu,GiShoppingCart} from "react-icons/gi";
 
 export default function Header() {
   return (
     <header className="App-header">
-        <GiHamburgerMenu className='menu'/>
+        <GiHamburgerMenu className='icons'/>
         <img  className='logo' src={Logo}/>
+        <GiShoppingCart className='icons'/>
       </header>
   )
 }
