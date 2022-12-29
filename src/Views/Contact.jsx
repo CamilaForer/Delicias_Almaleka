2f903658fd85848f769b54dae5a148fb2036f79c
Amanda Forero <amandaforero@outlook.com>
2022-12-28 21:28:25 -0500

    Contenido para vista de contacto

--- src/Views/Contact.jsx
+++ src/Views/Contact.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import Header from '../Components/Header/Header'
+import Form from 'react-bootstrap/Form';
+import Button from "react-bootstrap/Button";
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <div>
+      <Header/>
+      <p className="page_tittle">Contacto</p>
+      <p className='text-center'>Escribenos para pedidos especiales,realizamos diseños personalizados, desayunos, cajas sopresas y celebraciones especiales.</p>
+      <Form.Control
+          as="textarea"
+          placeholder="Deja tu mensaje aquí!"
+          style={{ height: '100px' }}
+        />
+      <Button variant="primary" >Enviar</Button>
+    </div>
   )
 }
 
