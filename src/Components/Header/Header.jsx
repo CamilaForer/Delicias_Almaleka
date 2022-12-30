67c85691dd920ee8b7f099709120e09dfbab398d
Amanda Forero <amandaforero@outlook.com>
2022-12-29 21:24:06 -0500

    Indicador de cantidad de productos en el carrito, redirección del logo a Home

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,20 +1,26 @@
-import React from 'react'
-import Logo from '../../../public/img/Delicias Almaleka.webp'
-import './Header.scss'
-import {GiShoppingCart} from "react-icons/gi";
+import React from "react";
+import Logo from "../../../public/img/Delicias Almaleka.webp";
+import "./Header.scss";
+import { GiShoppingCart } from "react-icons/gi";
 import { Link } from "react-router-dom";
-import Menu from './Menu';
+import Menu from "./Menu";
+import { useCart } from "../context/CartContext";
+
 
 export default function Header() {
-  
+  const context = useCart();
+  const cartContent = context.items;
   return (
     <header className="App-header">
-        <Menu/>
-        <img  className='logo' src={Logo}/>
-        <Link to='/Cart'>
-          <GiShoppingCart className='icons'/>
-        </Link>
-       
-      </header>
-  )
+      <Menu />
+      <a href="#/">
+        <img className="logo" src={Logo} />
+      </a>
+      <Link to="/Cart">
+        <span className="badge-cart" data-count={cartContent}>
+          <GiShoppingCart className="icons" />
+        </span>
+      </Link>
+    </header>
+  );
 }
