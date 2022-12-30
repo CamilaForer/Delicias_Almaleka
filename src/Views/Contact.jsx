87374a37de391546ac8c14d0cf372d67ee3a7af7
Amanda Forero <amandaforero@outlook.com>
2022-12-29 22:46:25 -0500

    Enviar comentario a whatsapp

--- src/Views/Contact.jsx
+++ src/Views/Contact.jsx
@@ -3,8 +3,14 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import {BsWhatsapp} from "react-icons/bs";
 import "../Styles/App.scss";
+import { useState } from "react";
+
 
 function Contact() {
+  const [message, setMessage] = useState("")
+  const sendMessage = () => {
+    window.open("https://api.whatsapp.com/send?phone=573173657520&text="+message, "_blank")
+  }
   return (
     <div>
       <Header />
@@ -15,11 +21,12 @@ function Contact() {
           desayunos, cajas sopresas y celebraciones especiales.
         </p>
         <Form.Control
+          onChange={(e)=>setMessage(e.target.value)}
           as="textarea"
           placeholder="¡Deja tu mensaje aquí!"
           style={{ height: "120px", width: "100%", marginBottom:"2%" }}
         />
-      <Button variant="primary">Enviar <BsWhatsapp/></Button>
+      <Button variant="primary" onClick={sendMessage}> Enviar <BsWhatsapp/></Button>
       </div>
     </div>
   );
